a50a3f20a5fed31cb76229729101bfa6446f8cff
Ahmed Yarub Hani Al Nuaimi <ahmedyarub@yahoo.com>
2021-09-08 00:21:50 -0300

    Remove redundant #undef directives

--- kubernetes/websocket/kube_exec.h
+++ kubernetes/websocket/kube_exec.h
@@ -7,9 +7,6 @@
 extern "C" {
 #endif
 
-#undef stdin
-#undef stdout
-
 int kube_exec(wsclient_t * wsc, const char *namespace_, const char *pod_name, const char *container_name, int stdin, int stdout, int tty, const char *command);
 
 #ifdef  __cplusplus
