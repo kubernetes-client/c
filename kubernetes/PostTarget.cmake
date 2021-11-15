985ffbecb2949a3d5d63a31d08efca2538298f3e
Ahmed Yarub Hani Al Nuaimi <ahmed.alnuaimi@zwift.com>
2021-11-15 11:50:39 -0300

    Automatically detect shared/static versions of libwebsockets

--- kubernetes/PostTarget.cmake
+++ kubernetes/PostTarget.cmake
@@ -1,2 +1,8 @@
-target_link_libraries(${pkgName} PRIVATE yaml websockets_shared)
+if (websockets_shared)
+    target_link_libraries(${WEBSOCKETS} websockets_shared)
+else()
+    target_link_libraries(${WEBSOCKETS} websockets)
+endif()
+
+target_link_libraries(${pkgName} PRIVATE yaml ${WEBSOCKETS})
 set_target_properties(${pkgName} PROPERTIES LINKER_LANGUAGE C)
