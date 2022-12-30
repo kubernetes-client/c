fc4f5d1f275cc5f1a3c758f393efa3f86674310b
Iacopo Rozzo <iacopo@sysdig.com>
2022-12-30 16:36:12 +0100

    Address review comments

--- kubernetes/config/incluster_config.c
+++ kubernetes/config/incluster_config.c
@@ -66,8 +66,7 @@ static int setBasePathInCluster(char **pBasePath)
 
     int basePathSize = strlen(SERVICE_HTTPS_PREFIX) + strlen(service_host_env) + strlen(service_port_env) + 2 /* 1 for ':', 1 for '\0' */ ;
     bool isIPv6 = false;
-    if (strchr(service_host_env, ':') == NULL)
-    {
+    if (strchr(service_host_env, ':') != NULL) {
         isIPv6 = true;
         // Takes into account the square brackets to escape the IP v6 address.
         basePathSize += 2;
